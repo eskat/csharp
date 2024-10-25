39cfd9ca7c4e02b65ea0126bb94641c165b0fa4f
eskat <skmoyls@gmail.com>
2024-10-25 09:57:46 -0700

    Make a change to a comment

--- SdetBootcampDay2/Exercises/Exercises02.cs
+++ SdetBootcampDay2/Exercises/Exercises02.cs
@@ -16,7 +16,7 @@ namespace SdetBootcampDay2.Exercises
             };
 
             /**
-             * TODO: Create a mock object representing the payment processor. Pass in Stripe
+             * DONE: Create a mock object representing the payment processor. Pass in Stripe
              * as the payment processor type. Set up the mock so that a call to PayFor() with
              * FIFA 24 and 10 as arguments returns false.
              */
@@ -26,7 +26,7 @@ namespace SdetBootcampDay2.Exercises
 
 
             /**
-             * TODO: Complete the test by creating a new OrderHandler, passing in the mock object
+             * DONE: Complete the test by creating a new OrderHandler, passing in the mock object
              * for the payment processor. Call the Order() method and then assert that the PayFor()
              * method of the OrderHandler returns false
              */
@@ -37,7 +37,7 @@ namespace SdetBootcampDay2.Exercises
             Assert.That(orderHandler.PayFor(OrderItem.FIFA_24, 10), Is.False);
 
             /**
-             * TODO: verify that the PayFor() method of the mock payment processor was called
+             * DONE: verify that the PayFor() method of the mock payment processor was called
              * exactly once with FIFA_24 and 10 as parameters.
              */
 
